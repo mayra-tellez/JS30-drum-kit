df9926010b26c50dd8393013cdd75653a78eb543
mayrant <mayrant2004@yahoo.com>
2022-05-10 17:51:21 -0500

    remove transition function, base project complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.addEventListener("keydown", function (e) {
+const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
   const keyElement = document.querySelector(`.key[data-key="${e.key}"]`);
 
@@ -8,5 +8,19 @@ window.addEventListener("keydown", function (e) {
   // starts audio over so that it plays on every key press
   audio.currentTime = 0;
   audio.play();
-  keyElement.classList.add("playing");
-})
+  keyElement.classList.add("playing")
+}
+
+const removeTransition = (e) => {
+  const keys = document.querySelectorAll(".key");
+
+  // skip it if it's not a transform
+  if (e.propertyName !== "transform") return;
+
+  for (let key of keys) {
+    key.classList.remove("playing");
+  }
+}
+
+window.addEventListener("keydown", playSound);
+window.addEventListener("transitionend", removeTransition);
